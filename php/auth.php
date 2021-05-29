3682ed5d53747f7768494877f6934b828da645e5
Tipa Koder <44733288+tipakoder@users.noreply.github.com>
2021-05-29 11:07:22 +0500

    Update auth.php

--- php/auth.php
+++ php/auth.php
@@ -9,7 +9,7 @@ if(isset($_GET['token'])){
     $token = $_GET['token'];
     if( $query = dbQueryOne("SELECT account.* FROM account, account_session WHERE account_session.session_key = '{$token}' AND account.id = account_session.account_id") ){
 
-        if($query['session_time'] > (time()-$session_standing)){
+        if((intval($query['session_time'])+$session_standing) > time()){
         	$level_access = ($query["type"] === "moderator") ? 1 : 0;
 	        $currentUser = $query;
 	        $auth = true;
