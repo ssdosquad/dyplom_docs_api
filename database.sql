6d9b8b9d3613f6776fb34bca2ff32b828223cc71
Pion4r <staryliss.nikita.2004@gmail.com>
2021-05-29 09:08:41 +0500

    Обновление базы данных.

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Май 21 2021 г., 14:27
+-- Время создания: Май 29 2021 г., 07:08
 -- Версия сервера: 8.0.19
 -- Версия PHP: 8.0.1
 
@@ -56,6 +56,64 @@ CREATE TABLE `account_session` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
+-- Дамп данных таблицы `account_session`
+--
+
+INSERT INTO `account_session` (`id`, `account_id`, `session_key`, `session_time`, `ip`) VALUES
+(1, 1, 'c2d19f32260783f94f5cfbd905682da8512491d9e19f52bf64de9c2076c25576', 1621596557, '127.0.0.1');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `category`
+--
+
+CREATE TABLE `category` (
+  `id` int NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `deal`
+--
+
+CREATE TABLE `deal` (
+  `id` int NOT NULL,
+  `fullname` varchar(600) NOT NULL,
+  `date_born` varchar(45) NOT NULL,
+  `passport_series` int NOT NULL,
+  `passport_id` int NOT NULL,
+  `passport_issued` varchar(120) NOT NULL,
+  `passport_date` int NOT NULL,
+  `short_text` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `deal_category`
+--
+
+CREATE TABLE `deal_category` (
+  `deal_id` int NOT NULL,
+  `category_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `deal_document`
+--
+
+CREATE TABLE `deal_document` (
+  `id` int NOT NULL,
+  `deal_id` int NOT NULL,
+  `path` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
 -- Индексы сохранённых таблиц
 --
 
@@ -73,6 +131,32 @@ ALTER TABLE `account_session`
   ADD KEY `account_id` (`account_id`);
 
 --
+-- Индексы таблицы `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `deal`
+--
+ALTER TABLE `deal`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `deal_category`
+--
+ALTER TABLE `deal_category`
+  ADD KEY `category_id` (`category_id`),
+  ADD KEY `deal_id` (`deal_id`);
+
+--
+-- Индексы таблицы `deal_document`
+--
+ALTER TABLE `deal_document`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `deal_id` (`deal_id`);
+
+--
 -- AUTO_INCREMENT для сохранённых таблиц
 --
 
@@ -86,6 +170,24 @@ ALTER TABLE `account`
 -- AUTO_INCREMENT для таблицы `account_session`
 --
 ALTER TABLE `account_session`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT для таблицы `category`
+--
+ALTER TABLE `category`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `deal`
+--
+ALTER TABLE `deal`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `deal_document`
+--
+ALTER TABLE `deal_document`
   MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
@@ -97,6 +199,19 @@ ALTER TABLE `account_session`
 --
 ALTER TABLE `account_session`
   ADD CONSTRAINT `account_session_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `account` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Ограничения внешнего ключа таблицы `deal_category`
+--
+ALTER TABLE `deal_category`
+  ADD CONSTRAINT `deal_category_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `deal_category_ibfk_2` FOREIGN KEY (`deal_id`) REFERENCES `deal` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Ограничения внешнего ключа таблицы `deal_document`
+--
+ALTER TABLE `deal_document`
+  ADD CONSTRAINT `deal_document_ibfk_1` FOREIGN KEY (`deal_id`) REFERENCES `deal` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
