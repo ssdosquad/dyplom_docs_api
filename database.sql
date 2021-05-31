8207f79270fdbaaac7e9fc0f0aabc2a4f7b90de6
Tipakoder <staryliss.nikita.2004@gmail.com>
2021-05-31 22:11:30 +0500

    Обновлена база данных

--- database.sql
+++ database.sql
@@ -1,219 +1,193 @@
--- phpMyAdmin SQL Dump
--- version 5.0.4
--- https://www.phpmyadmin.net/
+-- MySQL dump 10.13  Distrib 5.7.34, for Linux (x86_64)
 --
--- Хост: 127.0.0.1:3306
--- Время создания: Май 29 2021 г., 07:08
--- Версия сервера: 8.0.19
--- Версия PHP: 8.0.1
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: docs
+-- ------------------------------------------------------
+-- Server version	5.7.34-0ubuntu0.18.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- База данных: `docs`
+-- Table structure for table `account`
 --
 
--- --------------------------------------------------------
-
---
--- Структура таблицы `account`
---
-
+DROP TABLE IF EXISTS `account`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `account` (
-  `id` int NOT NULL,
-  `name` varchar(120) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `fullname` varchar(300) NOT NULL,
+  `position` varchar(120) NOT NULL,
+  `departament` varchar(120) NOT NULL,
   `login` varchar(45) NOT NULL,
-  `password` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Дамп данных таблицы `account`
+-- Dumping data for table `account`
 --
 
-INSERT INTO `account` (`id`, `name`, `login`, `password`) VALUES
-(1, 'User', 'user', '$2y$10$Dq5LK0DmUuMDZIcGVLX86O/CZP7vtcYDjhAgIbenjOhpA.JDUNKMK');
-
--- --------------------------------------------------------
+LOCK TABLES `account` WRITE;
+/*!40000 ALTER TABLE `account` DISABLE KEYS */;
+INSERT INTO `account` VALUES (1,'','','','user','$2y$10$Dq5LK0DmUuMDZIcGVLX86O/CZP7vtcYDjhAgIbenjOhpA.JDUNKMK');
+/*!40000 ALTER TABLE `account` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Структура таблицы `account_session`
+-- Table structure for table `account_session`
 --
 
+DROP TABLE IF EXISTS `account_session`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `account_session` (
-  `id` int NOT NULL,
-  `account_id` int NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `account_id` int(11) NOT NULL,
   `session_key` varchar(255) NOT NULL,
-  `session_time` int NOT NULL,
-  `ip` varchar(12) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `session_time` int(11) NOT NULL,
+  `ip` varchar(12) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `account_id` (`account_id`),
+  CONSTRAINT `account_session_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `account` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Дамп данных таблицы `account_session`
+-- Dumping data for table `account_session`
 --
 
-INSERT INTO `account_session` (`id`, `account_id`, `session_key`, `session_time`, `ip`) VALUES
-(1, 1, 'c2d19f32260783f94f5cfbd905682da8512491d9e19f52bf64de9c2076c25576', 1621596557, '127.0.0.1');
-
--- --------------------------------------------------------
+LOCK TABLES `account_session` WRITE;
+/*!40000 ALTER TABLE `account_session` DISABLE KEYS */;
+INSERT INTO `account_session` VALUES (1,1,'c2d19f32260783f94f5cfbd905682da8512491d9e19f52bf64de9c2076c25576',1621596557,'127.0.0.1');
+/*!40000 ALTER TABLE `account_session` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Структура таблицы `category`
+-- Table structure for table `category`
 --
 
+DROP TABLE IF EXISTS `category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `category` (
-  `id` int NOT NULL,
-  `name` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Структура таблицы `deal`
+-- Dumping data for table `category`
 --
 
+LOCK TABLES `category` WRITE;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+INSERT INTO `category` VALUES (1,'Представление интересов в суде'),(2,'Налоговые споры'),(3,'Составление исков, жалоб и документов правового порядка'),(4,'Регистрация ООО, ИП'),(5,'Исполнительное производство'),(6,'Трудовые споры'),(7,'Наследственные споры'),(8,'Семейное право'),(9,'Жилищные споры'),(10,'Согласовать перепланировку'),(11,'Оформить самострой'),(12,'Приватизация недвижимости'),(13,'Завершенные'),(14,'Другое');
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `deal`
+--
+
+DROP TABLE IF EXISTS `deal`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `deal` (
-  `id` int NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `account_id` int(11) DEFAULT NULL,
+  `status` enum('active','unactive') DEFAULT 'active',
   `fullname` varchar(600) NOT NULL,
   `date_born` varchar(45) NOT NULL,
-  `passport_series` int NOT NULL,
-  `passport_id` int NOT NULL,
+  `passport_series` int(11) NOT NULL,
+  `passport_id` int(11) NOT NULL,
   `passport_issued` varchar(120) NOT NULL,
-  `passport_date` int NOT NULL,
-  `short_text` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
+  `passport_date` int(11) NOT NULL,
+  `short_text` text NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `account_id` (`account_id`),
+  CONSTRAINT `deal_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `account` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Структура таблицы `deal_category`
+-- Dumping data for table `deal`
 --
 
+LOCK TABLES `deal` WRITE;
+/*!40000 ALTER TABLE `deal` DISABLE KEYS */;
+/*!40000 ALTER TABLE `deal` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `deal_category`
+--
+
+DROP TABLE IF EXISTS `deal_category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `deal_category` (
-  `deal_id` int NOT NULL,
-  `category_id` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
+  `deal_id` int(11) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  KEY `category_id` (`category_id`),
+  KEY `deal_id` (`deal_id`),
+  CONSTRAINT `deal_category_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`),
+  CONSTRAINT `deal_category_ibfk_2` FOREIGN KEY (`deal_id`) REFERENCES `deal` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Структура таблицы `deal_document`
+-- Dumping data for table `deal_category`
 --
 
+LOCK TABLES `deal_category` WRITE;
+/*!40000 ALTER TABLE `deal_category` DISABLE KEYS */;
+/*!40000 ALTER TABLE `deal_category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `deal_document`
+--
+
+DROP TABLE IF EXISTS `deal_document`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `deal_document` (
-  `id` int NOT NULL,
-  `deal_id` int NOT NULL,
-  `path` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `deal_id` int(11) NOT NULL,
+  `path` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `deal_id` (`deal_id`),
+  CONSTRAINT `deal_document_ibfk_1` FOREIGN KEY (`deal_id`) REFERENCES `deal` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Индексы сохранённых таблиц
+-- Dumping data for table `deal_document`
 --
 
---
--- Индексы таблицы `account`
---
-ALTER TABLE `account`
-  ADD PRIMARY KEY (`id`);
+LOCK TABLES `deal_document` WRITE;
+/*!40000 ALTER TABLE `deal_document` DISABLE KEYS */;
+/*!40000 ALTER TABLE `deal_document` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Индексы таблицы `account_session`
---
-ALTER TABLE `account_session`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `account_id` (`account_id`);
-
---
--- Индексы таблицы `category`
---
-ALTER TABLE `category`
-  ADD PRIMARY KEY (`id`);
-
---
--- Индексы таблицы `deal`
---
-ALTER TABLE `deal`
-  ADD PRIMARY KEY (`id`);
-
---
--- Индексы таблицы `deal_category`
---
-ALTER TABLE `deal_category`
-  ADD KEY `category_id` (`category_id`),
-  ADD KEY `deal_id` (`deal_id`);
-
---
--- Индексы таблицы `deal_document`
---
-ALTER TABLE `deal_document`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `deal_id` (`deal_id`);
-
---
--- AUTO_INCREMENT для сохранённых таблиц
---
-
---
--- AUTO_INCREMENT для таблицы `account`
---
-ALTER TABLE `account`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT для таблицы `account_session`
---
-ALTER TABLE `account_session`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT для таблицы `category`
---
-ALTER TABLE `category`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT для таблицы `deal`
---
-ALTER TABLE `deal`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT для таблицы `deal_document`
---
-ALTER TABLE `deal_document`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
-
---
--- Ограничения внешнего ключа сохраненных таблиц
---
-
---
--- Ограничения внешнего ключа таблицы `account_session`
---
-ALTER TABLE `account_session`
-  ADD CONSTRAINT `account_session_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `account` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-
---
--- Ограничения внешнего ключа таблицы `deal_category`
---
-ALTER TABLE `deal_category`
-  ADD CONSTRAINT `deal_category_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  ADD CONSTRAINT `deal_category_ibfk_2` FOREIGN KEY (`deal_id`) REFERENCES `deal` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-
---
--- Ограничения внешнего ключа таблицы `deal_document`
---
-ALTER TABLE `deal_document`
-  ADD CONSTRAINT `deal_document_ibfk_1` FOREIGN KEY (`deal_id`) REFERENCES `deal` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-COMMIT;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-05-31 13:29:37
