74841dee61f2efb17b8266d38133b9fd61004888
Tipakoder <staryliss.nikita.2004@gmail.com>
2021-06-09 12:13:18 +0500

    HOT FIX#9

--- php/word.php
+++ php/word.php
@@ -27,8 +27,8 @@ function ready_template_download($options, $template){
 	$temp_word_file = ROOTDIR."/tmp/".time()."_word.docx";
 	$document->saveAs($temp_word_file);
 	// Открываем временный файл для конвертации в PDF
-    $temp_html_file = ROOTDIR."/document/".$template."_".time().".html";
-    $temp_pdf_file = "/document/".time().".pdf";
+    $temp_html_file = ROOTDIR."/tmp/".time().".html";
+    $temp_pdf_file = "/document/".$template."_".time().".pdf";
     // Загружаем созданный DOCX файл для конвертации в HTML
     $phpWord = \PhpOffice\PhpWord\IOFactory::load($temp_word_file);
     $xmlWriter = \PhpOffice\PhpWord\IOFactory::createWriter($phpWord , 'HTML');
